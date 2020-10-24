b0427bf3e6797598c48adb3d9511d884fb302329
tanwar-abhi <abhishektanwar4790@gmail.com>
2020-10-25 01:36:11 +0530

    filter main file update

---
+++ filter.cpp
@@ -0,0 +1,9 @@
+// An improved image filter implemented in cpp that takes an image as input and gives a filtered image as output.
+
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+
+int main(int arc, char* argv[]){
+
+}
